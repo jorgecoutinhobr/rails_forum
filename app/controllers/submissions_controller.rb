8f26a59ba23bb3ca5ed0878e971dca3b8a476b07
Jorge Coutinho <jorgecsn@id.uff.br>
2024-08-27 17:38:17 -0300

    Add submission feature to application

---
+++ app/controllers/submissions_controller.rb
@@ -0,0 +1,47 @@
+class SubmissionsController < ApplicationController
+  before_action :set_submission, only: %i[ show edit update destroy ]
+
+  def index
+    @submissions = Submission.all
+  end
+
+  def show; end
+
+  def new
+    @submission = Submission.new
+  end
+
+  def edit; end
+
+  def create
+    @submission = Submission.new(submission_params)
+
+    if @submission.save
+      redirect_to submission_url(@submission), notice: "Submission was successfully created."
+    else
+        render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @submission.update(submission_params)
+      redirect_to submission_url(@submission), notice: "Submission was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @submission.destroy
+    redirect_to submissions_url, notice: "Submission was successfully destroyed."
+  end
+
+  private
+    def set_submission
+      @submission = Submission.find(params[:id])
+    end
+
+    def submission_params
+      params.require(:submission).permit(:title, :body, :url, :media, :user_id)
+    end
+end
